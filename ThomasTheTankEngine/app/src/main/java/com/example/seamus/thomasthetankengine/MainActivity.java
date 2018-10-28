ac61539a17efb80525b2b4482f1047c3c2319da0
Seamus Ferry <seamyferry@hotmail.co.uk>
2018-10-28 09:57:32 +0000

    ReleaseBranch

--- ThomasTheTankEngine/app/src/main/java/com/example/seamus/thomasthetankengine/MainActivity.java
+++ ThomasTheTankEngine/app/src/main/java/com/example/seamus/thomasthetankengine/MainActivity.java
@@ -1,61 +1,81 @@
 package com.example.seamus.thomasthetankengine;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static ImageView  imageView_thomasFace, imageView_thomasFaceSmile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView_thomasFace = (ImageView) findViewById(R.id.imageView_thomasFace);
+        imageView_thomasFaceSmile = (ImageView) findViewById(R.id.imageView_thomasFaceSmile);
+        imageView_thomasFace.setVisibility(View.VISIBLE);
+        imageView_thomasFaceSmile.setVisibility(View.GONE);
+
+
     }
 
     public void thomasFaceClick(View view) {
-            MediaPlayer mediaPlayer = new MediaPlayer();
-            try
-            {
-                mediaPlayer.setDataSource("gs://thomas-656d9.appspot.com/thomasanthem.mp3");
-                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared(MediaPlayer mp) {
-                        mp.start();
-                    }
-                });
+        try {
+            MediaPlayer player = new MediaPlayer();
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            player.setDataSource("https://firebasestorage.googleapis.com/v0/b/thomas-656d9.appspot.com/o/Thomas%20New%20Theme%20Tune.mp3?alt=media&token=5b16707c-3313-4364-8c8c-fbde33327bff");
+            player.prepare();
+            player.start();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        imageView_thomasFace.setVisibility(View.GONE);
+        imageView_thomasFaceSmile.setVisibility(View.VISIBLE);
 
-                mediaPlayer.prepare();
-         }catch (IOException e)
-            {
+    }
 
-                e.printStackTrace();
-            }
+
+    public void thomasFaceSmileClick(View view) {
+        try {
+            MediaPlayer player = new MediaPlayer();
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            player.setDataSource("https://firebasestorage.googleapis.com/v0/b/thomas-656d9.appspot.com/o/Thomas%20New%20Theme%20Tune.mp3?alt=media&token=5b16707c-3313-4364-8c8c-fbde33327bff");
+            player.prepare();
+            player.start();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        imageView_thomasFace.setVisibility(View.VISIBLE);
+        imageView_thomasFaceSmile.setVisibility(View.GONE);
+    }
+
+    public void thomasFaceClick2(View view) {
+
 
 
     }
 
-    public void play_song(View view) {
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        try
-        {
-            mediaPlayer.setDataSource("gs://thomas-656d9.appspot.com/thomasanthem.mp3");
-            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    mp.start();
-                }
-            });
-
-            mediaPlayer.prepare();
-        }catch (IOException e)
-        {
-
-            e.printStackTrace();
-        }
+    public void thomasFaceSmileClick2(View view) {
 
 
+
+    }
+
+    public void thomasFaceClick3(View view) {
+    }
+
+    public void thomasFaceClick4(View view) {
+    }
+
+    public void thomasFaceSmileClick3(View view) {
+    }
+
+    public void thomasFaceSmileClick4(View view) {
     }
 }
